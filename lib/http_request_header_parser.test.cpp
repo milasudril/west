e19be007e1043f7b9357c8c67d36d87c87575602
Torbjörn Rathsman <milasudril@gmail.com>
2023-05-07 16:30:37 +0200

    Trigger all good cases

--- lib/http_request_header_parser.test.cpp
+++ lib/http_request_header_parser.test.cpp
@@ -11,16 +11,56 @@ TESTCASE(west_http_request_header_parser_parse_complete_header)
 "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0\r\n"
 "Accept: text/html,application/xhtml+xml,application/xml;\r\n"
 "\t q=0.9,image/avif,image/webp,*/*;\r\n"
+" \r\n"
 " \tq=0.8\r\n"
 "Accept-Language: sv-SE,sv;q=0.8,en-US;q=0.5,en;q=0.3\r\n"
 "Accept-Encoding: gzip, deflate, br\r\n"
 "DNT: 1\r\n"
 "Connection: keep-alive\r\n"
+"Key-without-value-1:   \r\n"
+"Key-without-value-2:\r\n"
 "Upgrade-Insecure-Requests: 1\r\n"
 "Sec-Fetch-Dest: document\r\n"
 "Sec-Fetch-Mode: navigate\r\n"
 "Sec-Fetch-Site: none\r\n"
 "Sec-Fetch-User: ?1\r\n"
+"\r\nSome additional data"};
+
+	west::http::request_header header{};
+	west::http::request_header_parser parser{header};
+
+	auto res = parser.parse(serialized_header);
+	EXPECT_EQ(res.ec, west::http::req_header_parser_error_code::completed);
+
+	EXPECT_EQ(header.request_line.method, "GET");
+	EXPECT_EQ(header.request_line.request_target, "/");
+	EXPECT_EQ(header.request_line.http_version.major(), 1);
+	EXPECT_EQ(header.request_line.http_version.minor(), 1);
+	EXPECT_EQ(std::string_view{res.ptr}, "Some additional data");
+}
+
+TESTCASE(west_http_request_header_parser_parse_no_fields)
+{
+	std::string_view serialized_header{"GET / HTTP/1.1\r\n"
+"\r\nSome additional data"};
+
+	west::http::request_header header{};
+	west::http::request_header_parser parser{header};
+
+	auto res = parser.parse(serialized_header);
+	EXPECT_EQ(res.ec, west::http::req_header_parser_error_code::completed);
+
+	EXPECT_EQ(header.request_line.method, "GET");
+	EXPECT_EQ(header.request_line.request_target, "/");
+	EXPECT_EQ(header.request_line.http_version.major(), 1);
+	EXPECT_EQ(header.request_line.http_version.minor(), 1);
+	EXPECT_EQ(std::string_view{res.ptr}, "Some additional data");
+}
+
+TESTCASE(west_http_request_header_parser_parse_last_field_has_no_value)
+{
+	std::string_view serialized_header{"GET / HTTP/1.1\r\n"
+"a-field:\r\n"
 "\r\nSome additional data"};
 
 	west::http::request_header header{};
