3b603b05858e8b01df9ca989c62cc970a1765028
Torbjörn Rathsman <milasudril@gmail.com>
2023-05-25 20:57:40 +0200

    Add testcase for failed write

--- lib/http_write_response_body.test.cpp
+++ lib/http_write_response_body.test.cpp
@@ -22,6 +22,19 @@ namespace
 		std::string output;
 	};
 
+	struct bad_sink
+	{
+		west::io::write_result write(std::span<char const>)
+		{
+			return west::io::write_result{
+				0,
+				west::io::operation_result::operation_would_block
+			};
+		}
+
+		std::string output;
+	};
+
 	enum class error_code{no_error};
 
 	struct read_result
@@ -99,4 +112,34 @@ TESTCASE(http_write_response_body_write_all_data)
 	EXPECT_EQ(res.http_status, west::http::status::ok);
 	EXPECT_EQ(res.error_message, nullptr);
 	EXPECT_EQ(session.connection.output, src);
+}
+
+TESTCASE(http_write_response_body_write_failed)
+{
+	std::array<char, 4096> buffer{};
+	west::io_adapter::buffer_span buff_sapn{buffer};
+
+	std::string_view src{
+"Etiam egestas ex laoreet tortor tristique, vitae tristique enim vehicula. Aenean mollis tristique "
+"eros nec malesuada. Suspendisse bibendum maximus erat, id volutpat enim. Phasellus at pharetra "
+"elit, ut molestie velit. Phasellus sed lacinia risus. Pellentesque condimentum, arcu at ultricies "
+"egestas, dui lectus fringilla dui, et pellentesque lorem enim at arcu. Pellentesque sed tortor a "
+"tortor sollicitudin dapibus nec sed nulla. Nulla laoreet vel eros ut venenatis. Praesent aliquam "
+"pharetra scelerisque. Fusce ut ex fermentum, laoreet ex ut, sollicitudin metus. Praesent luctus "
+"purus at eleifend ullamcorper. Integer viverra ipsum enim, eu sollicitudin diam rhoncus in. "
+};
+
+	west::http::session session{bad_sink{},
+		request_handler{src},
+		west::http::request_header{},
+		west::http::response_header{}
+	};
+
+	west::http::write_response_body writer{std::size(src)};
+
+	auto res = writer(buff_sapn, session);
+
+	EXPECT_EQ(res.status, west::http::session_state_status::more_data_needed);
+	EXPECT_EQ(res.http_status, west::http::status::ok);
+	EXPECT_EQ(res.error_message, nullptr);
 }
