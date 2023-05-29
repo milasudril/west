f3eaa35bcf7facfd0e7c21cd05e8a468877ad13f
Torbjörn Rathsman <milasudril@gmail.com>
2023-05-29 21:19:45 +0200

    More work on fd_vent_monitor

--- lib/io_fd_event_monitor.hpp
+++ lib/io_fd_event_monitor.hpp
@@ -2,10 +2,13 @@
 #define WEST_IO_FD_EVENT_MONITOR_HPP
 
 #include "./system_error.hpp"
-#include "./utils.hpp"
 #include "./io_fd.hpp"
 
 #include <sys/epoll.h>
+
+#include <vector>
+#include <unordered_map>
+#include <span>
 
 namespace west::io
 {
@@ -17,6 +20,7 @@ namespace west::io
 		{x()} -> std::same_as<fd_event_result>;
 	};
 
+	template<fd_event_listener FdEventListener>
 	class fd_event_monitor
 	{
 	public:
@@ -26,30 +30,55 @@ namespace west::io
 			{ throw system_error{"Failed to create epoll instance", errno}; }
 		}
 
-		template<fd_event_listener Listener>
-		fd_event_monitor& add(struct fd fd, Listener&& l)
+		void wait_for_events()
 		{
-			add_listener(fd, fd_listener{
-				.closure = make_type_erased_ptr<Listener>(std::forward<Listener>(l)),
-				.callback = [](void* object) {
-					return (*static_cast<Listener*>(object))();
-				},
-				.fd = fd
-			});
+			auto const n = ::epoll_wait(m_fd.get(), std::data(m_events), static_cast<int>(std::size(m_events)), -1);
+
+			if(n == -1)
+			{ throw system_error{"epoll_wait failed", errno}; }
+
+			for(auto& event : std::span{std::begin(m_events), static_cast<size_t>(n)})
+			{
+				auto const data = static_cast<event_data*>(event.data.ptr);
+				if(data->listener() == fd_event_result::remove_listener)
+				{
+					epoll_event event{};
+					::epoll_ctl(m_fd.get(), EPOLL_CTL_DEL, data->fd, &event);
+					m_events.erase(data->fd);
+				}
+			}
+		}
+
+		fd_event_monitor& add(struct fd fd, FdEventListener&& l)
+		{
+			assert(!m_listeners.contains(fd));
+
+			auto const i = m_listeners.insert(std::pair{fd, event_data{std::move(l), fd}});
+			epoll_event event{
+				.events = EPOLLIN | EPOLLOUT,
+				.data = &i.first->second
+			};
+
+			if(::epoll_ctl(m_fd.get(), EPOLL_CTL_ADD, fd, &event) == -1)
+			{
+				auto const saved_errno = errno;
+				m_listeners.erase(i);
+				throw system_error{"Failed to add event listener for file descriptor", saved_errno};
+			}
+
+			m_events.resize(std::size(m_listeners));
 			return *this;
 		}
 
 	private:
 		fd_owner m_fd;
-
-		struct fd_listener
+		struct event_data
 		{
-			type_erased_ptr closure;
-			fd_event_result (*callback)(void* object);
-			struct fd fd;
+			FdEventListener listener;
+			struct fd const fd;
 		};
-
-		void	add_listener(struct fd, fd_listener);
+		std::vector<epoll_event> m_events;
+		std::unordered_map<fd, event_data> m_listeners;
 	};
 }
 
